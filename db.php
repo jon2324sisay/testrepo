9c6471d1f1b820b5a1bbc4747828bbfbbe362069
jon2324sisay <abebechsisayamare@gmail.com>
2026-08-24 16:47:55 +0300

    Hide warnings in db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 $host = "sql202.infinityfree.com";
 $username = "if0_42712329";
 $password = "8FzYyQMhFq85A";
