3cf8c5b10ee32a7c7d98d63e6a893e044bdd4edb
jon2324sisay <abebechsisayamare@gmail.com>
2026-08-24 17:04:19 +0300

    Fix database connection in db.php

--- db.php
+++ db.php
@@ -2,7 +2,6 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 $host = "sql202.infinityfree.com";
-...
 $username = "if0_42712329";
 $password = "8FzYyQMhFq85A";
 $dbname = "if0_42712329_office";
